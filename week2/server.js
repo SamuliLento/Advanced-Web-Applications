897d319070407e7592dda39416e7a0bc67e8ff15
SamuliLento <samuli.lento@gmail.com>
2022-11-14 13:23:16 +0200

    week2

---
+++ week2/server.js
@@ -0,0 +1,15 @@
+
+let hello = {
+    msg: "Hello world"
+};
+
+const express = require("express");
+const os = require("os");
+const app = express();
+const port = 3000;
+
+app.get("/hello", (req, res) => {
+    res.json(hello);
+});
+
+app.listen(port, () => console.log(`Server listening a port ${port}!`))
