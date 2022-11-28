48d1883af79c330cf8d611d615dd3fd877b82b44
SamuliLento <samuli.lento@gmail.com>
2022-11-28 14:39:30 +0200

    commit

--- week4/routes/index.js
+++ week4/routes/index.js
@@ -3,7 +3,23 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Recipes' });
+
+  fetch("http://localhost:3000/recipe/pizza")
+  .then(response => response.json())
+
+  console.log(response);
+
+  res.render('index', response)
+});
+
+router.get('/recipe/:food', function(req, res, next) {
+  res.send({
+      name: req.params.food,
+      ingredients: ["frozen pizza base", "tomato sauce", "mozzarellas"],
+      instructions: ["preheat oven 225C", "slice mozzarella", "put tomato sauce on pizza base",
+  "put mozzarella slices on pizza", "bake for 15 min"]
+  });
 });
 
 module.exports = router;
