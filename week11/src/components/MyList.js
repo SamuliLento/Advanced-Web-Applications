d52d131a47c65c6133895a8e1055df4cf2d90ee3
SamuliLento <samuli.lento@gmail.com>
2023-02-13 14:31:19 +0200

    m

--- week11/src/components/MyList.js
+++ week11/src/components/MyList.js
@@ -1,14 +1,14 @@
 function MyList (props) {
+
     return (
         <div>
             <h1>{props.header}</h1>
             <ol>
-                <li>
-                    {props.items[0].text}
-                </li>
-                <li>
-                    {props.items[1].text}
-                </li>
+                {props.items.map((item) => (
+                    <li>
+                        {item.text}
+                    </li>
+                ))}
             </ol>
         </div>
     )
