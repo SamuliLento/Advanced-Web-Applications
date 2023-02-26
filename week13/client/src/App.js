822528275cea19706838a4e02a742aac2d76c7f7
SamuliLento <samuli.lento@gmail.com>
2023-02-26 15:02:12 +0200

    commit

--- week13/client/src/App.js
+++ week13/client/src/App.js
@@ -1,11 +1,40 @@
 import './App.css';
+import {useState} from 'react'
 
 function App() {
-  return (
-    <div className="App">
-      <h1>Books</h1>
-    </div>
-  );
+
+    const [bookData, setBookData] = useState({})
+
+    const submit = (e) => {
+        e.prevetDefault()
+        console.log("Getting here")
+
+        fetch("/api/book/", {
+            method: "POST",
+            headers: {
+                "Content-type": "application/json"
+            },
+            body: JSON.stringify(bookData),
+            mode: "cors"
+        })
+            .then(response => response.json())
+    }
+
+    const handleChange = (e) => {
+        setBookData({...bookData, [e.target.id]: e.target.value})
+    }
+
+    return (
+        <div className="App">
+            <h1>Books</h1>
+            <form onSubmit={submit} onChange={handleChange}>
+                <input id="name" type="string"></input>
+                <input id="author" type="string"></input>
+                <input id="pages" type="number"></input>
+                <input id="submit" type="submit"></input>
+            </form>
+        </div>
+    );
 }
 
 export default App;
