b97f11931474c028a2d86bb55313af0f2c5ccf8a
SamuliLento <samuli.lento@gmail.com>
2023-02-26 15:23:46 +0200

    commit

--- week13/client/src/App.js
+++ week13/client/src/App.js
@@ -1,39 +1,17 @@
 import './App.css';
-import {useState} from 'react'
+import Index from './components/Index';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 
 function App() {
-
-    const [bookData, setBookData] = useState({})
-
-    const submit = (e) => {
-        e.prevetDefault()
-        console.log("Getting here")
-
-        fetch("/api/book/", {
-            method: "POST",
-            headers: {
-                "Content-type": "application/json"
-            },
-            body: JSON.stringify(bookData),
-            mode: "cors"
-        })
-            .then(response => response.json())
-    }
-
-    const handleChange = (e) => {
-        setBookData({...bookData, [e.target.id]: e.target.value})
-    }
-
     return (
-        <div className="App">
-            <h1>Books</h1>
-            <form onSubmit={submit} onChange={handleChange}>
-                <input id="name" type="string"></input>
-                <input id="author" type="string"></input>
-                <input id="pages" type="number"></input>
-                <input id="submit" type="submit"></input>
-            </form>
-        </div>
+        <Router>
+            <div className="App">
+                <h1>Books</h1>
+                <Routes>
+                    <Route path="/" element={ <Index /> } />
+                </Routes>
+            </div>
+        </Router>
     );
 }
 
